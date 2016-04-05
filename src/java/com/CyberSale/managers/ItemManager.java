68436611fe96b1941b56a756edc5670c38d4edad
Braeden Sebastian <jbraeden@vt.edu>
2016-04-05 15:09:52 -0400

    jbraeden - created item and user manager classes

---
+++ src/java/com/CyberSale/managers/ItemManager.java
@@ -0,0 +1,20 @@
+/*
+ * Created by Joseph Sebastian on 2016.04.05  * 
+ * Copyright © 2016 Osman Balci. All rights reserved. * 
+ */
+package com.CyberSale.managers;
+
+import java.io.Serializable;
+import javax.enterprise.context.SessionScoped;
+import javax.inject.Named;
+
+/**
+ *
+ * @author Braeden
+ */
+@Named(value = "itemManager")
+@SessionScoped
+public class ItemManager implements Serializable
+{
+    
+}
---
+++ src/java/com/CyberSale/managers/UserManager.java
@@ -0,0 +1,19 @@
+/*
+ * Created by Joseph Sebastian on 2016.04.05  * 
+ * Copyright © 2016 Osman Balci. All rights reserved. * 
+ */
+package com.CyberSale.managers;
+
+import java.io.Serializable;
+import javax.enterprise.context.SessionScoped;
+import javax.inject.Named;
+
+/**
+ *
+ * @author Braeden
+ */
+@Named(value = "itemManager")
+@SessionScoped
+public class UserManager implements Serializable{
+    
+}
