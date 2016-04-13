b78290fa2fe6f93e388a9085048387c37f3642c0
Ryan Asper <rpasper@vt.edu>
2016-04-13 17:28:40 -0400

    Modified Header to float along top of page.
    Added javascript libraries to repository.

---
+++ web/resources/javascript/floating-header.js
@@ -0,0 +1,15 @@
+/* 
+ * Created by Ryan Asper on 2016.04.13  * 
+ * Copyright © 2016 Ryan Asper. All rights reserved. * 
+ */
+
+var name = "#floatHeader";
+var menuYloc = null;
+ 
+$(document).ready(function(){
+    menuYloc = parseInt($(name).css("top").substring(0,$(name).css("top").indexOf("px")));
+    $(window).scroll(function () {
+        var offset = menuYloc + $(document).scrollTop() + "px";
+        $(name).animate({top:offset},{duration:350,queue:false});
+    });
+});
