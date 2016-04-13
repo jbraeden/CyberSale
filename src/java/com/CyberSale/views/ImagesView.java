403957d6dabdd1e4392367a927e902ac0bd47413
cloud <cloud@172.31.200.112>
2016-04-13 13:06:36 -0400

    Revert "Home Page UI finalized."
    This reverts commit 447371f55e95af713e1f5eba626f62031fedb2ce.

--- src/java/com/CyberSale/views/ImagesView.java
+++ src/java/com/CyberSale/views/ImagesView.java
@@ -21,7 +21,7 @@ public class ImagesView {
     @PostConstruct
     public void init() {
         images = new ArrayList<String>();
-        for (int i = 2; i <= 2; i++) {
+        for (int i = 1; i <= 2; i++) {
             images.add("banner" + i + ".png");
         }
     }
