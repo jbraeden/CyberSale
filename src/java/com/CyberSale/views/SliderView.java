0ff6c085511fde6b86877ffaa12a73059f2a9332
SWAT Loaner <SWAT Loaner@T725SWAT032316>
2016-04-19 11:17:53 -0400

    Added slider to header for distance calculation

---
+++ src/java/com/CyberSale/views/SliderView.java
@@ -0,0 +1,19 @@
+/*
+ */
+package com.CyberSale.views;
+
+import javax.faces.bean.ManagedBean;
+ 
+@ManagedBean
+public class SliderView {
+    
+    private int number1 = 10;   
+ 
+    public int getNumber1() {
+        return number1;
+    }
+ 
+    public void setNumber1(int number1) {
+        this.number1 = number1;
+    }
+}
