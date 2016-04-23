cbb86fee5314e8788906c193fe83335574910fab
SWAT Loaner <SWAT Loaner@T725SWAT032316>
2016-04-23 16:33:42 -0400

    Made header fixed

--- web/resources/javascript/floating-header.js
+++ web/resources/javascript/floating-header.js
@@ -3,13 +3,13 @@
  * Copyright © 2016 Ryan Asper. All rights reserved. * 
  */
 
-var name = "#floatHeader";
-var menuYloc = null;
- 
-$(document).ready(function(){
-    menuYloc = parseInt($(name).css("top").substring(0,$(name).css("top").indexOf("px")));
-    $(window).scroll(function () {
-        var offset = menuYloc + $(document).scrollTop() + "px";
-        $(name).animate({top:offset},{duration:350,queue:false});
-    });
-});
+//var name = "#floatHeader";
+//var menuYloc = null;
+// 
+//$(document).ready(function(){
+//    menuYloc = parseInt($(name).css("top").substring(0,$(name).css("top").indexOf("px")));
+//    $(window).scroll(function () {
+//        var offset = menuYloc + $(document).scrollTop() + "px";
+//        $(name).animate({top:offset},{duration:0,queue:false});
+//    });
+//});
