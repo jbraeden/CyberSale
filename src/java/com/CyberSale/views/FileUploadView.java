dd3f98886d43d85a90c8e5cd34a28f1a19c9c079
Joe Fletcher <joevt@vt.edu>
2016-04-29 21:53:45 -0400

    Forgot one last thing in one file...Should be done with comments now

--- src/java/com/CyberSale/views/FileUploadView.java
+++ src/java/com/CyberSale/views/FileUploadView.java
@@ -1,7 +1,6 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+ * Created by Joe Fletcher on 2016.04.09  * 
+ * Copyright © 2016 Joe Fletcher. All rights reserved. * 
  */
 package com.CyberSale.views;
 
