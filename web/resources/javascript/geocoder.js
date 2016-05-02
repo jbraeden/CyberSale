3bd5e25245dfcbd5ce622fa758deb30a031f86ca
Braeden Sebastian <jbraeden@vt.edu>
2016-05-02 01:24:25 -0400

    Update geocoder.js
    Added comments

--- web/resources/javascript/geocoder.js
+++ web/resources/javascript/geocoder.js
@@ -17,9 +17,10 @@ var rad = function(x) {
 
 /*
  * Implementation of Haversine formula 
+ * Source: http://stackoverflow.com/questions/27928/calculate-distance-between-two-latitude-longitude-points-haversine-formula  
  */
 function getDistance(lat1, long1, lat2, long2) {
-  var R = 6378137; // Earth’s mean radius in meter
+  var R = 6378137;
   var dLat = rad(lat2 - lat1);
   var dLong = rad(long2 - long1);
   var a = Math.sin(dLat / 2) * Math.sin(dLat / 2) +
@@ -30,15 +31,21 @@ function getDistance(lat1, long1, lat2, long2) {
   return d * 0.000621371; // returns the distance in meter
 }
 
+/**
+ * Geocode all items in the search results list 
+ */
 function geocodeList() {
     var items = _itemList.getElementsByTagName('li');
+    // Iterate through each item in the search results
     for (var i = 0; i < items.length; i++) {
         var element = items[i].getElementsByClassName('result-zipcode')[0];
         var zipcode = element.innerHTML;
+        // Extract the zipcode and send to Google API for geocoding
         geocoder.geocode({'address': zipcode}, function(element, results, status) {
             if (status === google.maps.GeocoderStatus.OK) {
               var lat = results[0].geometry.location.lat();
               var long = results[0].geometry.location.lng();
+              // Calculate distances between current and desired locations 
               var dist = getDistance(_currentLat, _currentLong, lat, long)
               element.innerHTML += ' (' + dist.toFixed(2) + ' miles away)'
             } else {
@@ -48,6 +55,9 @@ function geocodeList() {
     }
 } 
 
+/**
+ * Ensure all elements of search list are within current current radius
+ */
 function updateList() {
   var label = document.querySelector('.sliderLabel');
   var sliderValue = Number(label.innerHTML.split(' ')[0]);
@@ -59,12 +69,15 @@ function updateList() {
   var resultsFound = false;
   _sliderval = sliderValue;
 
+  // Iterate through all items in list 
   var items = _itemList.getElementsByTagName('li');
   for (var i = 0; i < items.length; i++) {
       var distance = items[i].getElementsByClassName('result-zipcode')[0].innerText.split('(')[1].split(' ')[0];
       if (Number(distance) > _sliderval) {
+          // Make list item hidden
           items[i].style.display = 'none';
       } else {
+          // Make list item visible
           items[i].style.display = 'block';
           resultsFound = true;
       }
@@ -75,6 +88,9 @@ function updateList() {
   }
 }
 
+/**
+ * Granted access to browser location data, respond accordingly
+ */
 function locationAvailable(currentLocation) { 
     // Read current location
     _currentLat = currentLocation.coords.latitude;
@@ -92,13 +108,20 @@ function locationAvailable(currentLocation) {
     geocodeList();
 }
 
+/**
+ * No access to browser location data, no further action needed
+ */
 function locationBlocked() {
     console.log('Blocked');
 }
 
+/**
+ * Link event listeners upon page load
+ */
 window.addEventListener('load', function() {
     _radiusSlider = document.querySelector('.ui-slider-handle');
     _itemList = document.querySelector('.ui-datascroller-list');
+    // Prompt user for current location
     if(navigator.geolocation) {
         browserSupportFlag = true;
         navigator.geolocation.getCurrentPosition(locationAvailable, locationBlocked);
